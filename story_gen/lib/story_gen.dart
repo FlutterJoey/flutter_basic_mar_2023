aec3d10f32d252c0ba0bf91343c5cb4ecb530429
Joey Boerwinkel <joey@iconica.nl>
2023-03-06 10:54:07 +0100

    Fix sdk to be compatible with dart version on windows

--- story_gen/lib/story_gen.dart
+++ story_gen/lib/story_gen.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library story_gen;
+
+library;
 
 export 'src/story_gen_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
