05c1acf435be3e8413018205c2d6d27b956b83cc
Joey Boerwinkel <joey@iconica.nl>
2023-03-06 12:44:57 +0100

    Add language demo

---
+++ demos/language_demo/bin/language_demo.dart
@@ -0,0 +1,44 @@
+void main(List<String> arguments) {
+  var a = 2;
+  var b = 5;
+
+  print('The value of a = $a');
+  print('A($a) + B($b) = ${a + b}');
+
+  String story = 'Dit is regel een\n'
+      'Dit is regel twee\n'
+      'Dit is de rest.';
+  print(story);
+
+  const moodKey = 'mood';
+
+  var myMap = <String, String>{
+    'name': 'Vick',
+    moodKey: 'Slaperig',
+  };
+
+  print(myMap[moodKey]);
+
+  print(helloWithNamed());
+  print(helloWithNamed(name: 'Joey'));
+
+  var list = ['apples', 'bananas', 'oranges'];
+  list.forEach((item) {
+    print('${list.indexOf(item)}: $item');
+  });
+
+  num myNum = 1;
+  if (myNum is int) {
+    int myInt = myNum;
+  }
+
+  
+}
+
+String myFunction(String name) {
+  return 'Hello I am $name';
+}
+
+String helloWithNamed({String name = 'Unknown'}) {
+  return 'Hello $name';
+}
---
+++ demos/language_demo/test/language_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:language_demo/language_demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
